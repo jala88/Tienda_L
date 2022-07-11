e2dbfd9d0f9341a62ed8a13f5091871187ff15f7
jala88 <cbarquero70286@ufide.ac.cr>
2022-07-10 22:28:45 -0600

    Se realiza el commit de nuevo debido a problemas con netbeans, clase #5y #8

--- src/main/java/com/tienda/controller/IndexController.java
+++ src/main/java/com/tienda/controller/IndexController.java
@@ -1,11 +1,10 @@
 package com.tienda.controller;
 
-import com.tienda.domain.Cliente;
+import com.tienda.service.ArticuloService;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
-import com.tienda.service.ClienteService;
 import java.util.Arrays;
 import org.springframework.beans.factory.annotation.Autowired;
 
@@ -14,45 +13,21 @@ import org.springframework.beans.factory.annotation.Autowired;
 public class IndexController {
     
     @Autowired
-    private ClienteService clienteService;
+    private ArticuloService articuloService;
     
     
     @GetMapping("/")
     public String inicio(Model model){
         log.info("Ahora utilizamos MVC");
-        var clientes=clienteService.getClientes();
-        model.addAttribute("clientes", clientes);
+        var articulos=articuloService.getArticulos(true);
+        model.addAttribute("articulos", articulos);
         
-        /*Cliente cliente = new Cliente ("Anthony" , "Aguilera" , "anthonyag7@hotmail.com" , "85839617");
-        Cliente cliente2 = new Cliente ("Pandolfo" , "Ramos" , "anthonyag7@hotmail.com" , "85839617");
-        var clientes = Arrays.asList(cliente,cliente2);
-        model.addAttribute("clientes",clientes);*/
+        /*Cliente articulo = new Cliente ("Anthony" , "Aguilera" , "anthonyag7@hotmail.com" , "85839617");
+        Cliente articulo2 = new Cliente ("Pandolfo" , "Ramos" , "anthonyag7@hotmail.com" , "85839617");
+        var articulos = Arrays.asList(articulo,articulo2);
+        model.addAttribute("articulos",articulos);*/
         return "index";
     }
     
-    @GetMapping("/nuevoCliente")
-    public String nuevoCliente(Cliente cliente){
-        log.info("Ingresando nuevo cliente");
-        return "modificarCliente";
-    }
     
-    @PostMapping("/guardarCliente")
-    public String guardarCliente (Cliente cliente){
-        log.info("Guardando informacion sobre un cliente");
-        clienteService.save(cliente);
-        return "redirect:/";
-    }
-    
-    @GetMapping("/modificarCliente/{idCliente}")
-    public String modificarCliente(Cliente cliente, Model model){
-        cliente = clienteService.getCliente(cliente);
-        model.addAttribute("cliente", cliente);
-        return "modificarCliente";
-    }
-    
-    @GetMapping("/eliminarCliente/{idCliente}")
-    public String eliminarCliente(Cliente cliente, Model model){
-        clienteService.delete(cliente);
-        return "redirect:/";
-    }
 }
